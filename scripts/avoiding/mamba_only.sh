b24f1ffb7659227b0224838f9be3f4e2518e5f1c
xiaogangjia <jia266163@gmail.com>
2024-06-01 21:24:17 +0200

    add slurm

--- scripts/avoiding/mamba_only.sh
+++ scripts/avoiding/mamba_only.sh
@@ -3,5 +3,5 @@ python run.py --config-name=avoiding_config \
               agent_name=ddpm_mamba_only \
               group=avoiding_ddpm_mamba_only_seeds \
               mamba_ssm_cfg.d_state=8,16 \
-              mamba_ssm_cfg.d_conv=2,4 \
+              mamba_ssm_cfg.d_conv=4 \
               seed=0,1,2,3,4,5
