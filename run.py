44bf3b8e748c9e210fab6e3a15b3cdf61c575be9
utfvv@student.kit.edu <utfvv@uc2n996.localdomain>
2024-09-07 14:49:18 +0200

    add checkpoint

--- run.py
+++ run.py
@@ -46,13 +46,20 @@ def main(cfg: DictConfig) -> None:
         project=cfg.wandb.project,
         entity=cfg.wandb.entity,
         group=cfg.group,
-        mode="disabled",
+        mode="online",
         config=wandb.config
     )
-
     agent = hydra.utils.instantiate(cfg.agents)
+    if cfg.load_checkpoint:
+        checkpoint_path = cfg.checkpoint_dir
+        checkpoint_name = cfg.checkpoint_name
+        if os.path.exists(os.path.join(checkpoint_path, checkpoint_name)):
+            agent.load_pretrained_model(checkpoint_path, sv_name=checkpoint_name)
+            log.info(f"Loaded pretrained model from {os.path.join(checkpoint_path, checkpoint_name)}")
+        else:
+            log.warning(f"Checkpoint {os.path.join(checkpoint_path, checkpoint_name)} not found. Starting from scratch.")
     # train the agent
-    agent.train_agent()
+    agent.train_vision_agent()
 
     # load the model performs best on the evaluation set
     # agent.load_pretrained_model(agent.working_dir, sv_name=agent.eval_model_name)
